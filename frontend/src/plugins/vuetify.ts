fdc3d674617ecfb5f5b62daf7c112a6b45c435a4
trak2026z <trak2026z@gmail.com>
2025-08-16 06:11:24 +0200

    vuetify

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -1,12 +1,34 @@
 // src/plugins/vuetify.ts
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Ikony MDI
+import '@mdi/font/css/materialdesignicons.css'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
+// DataTable w Vuetify 3.9.x jest w labs – trzeba zarejestrować
+import { VDataTable } from 'vuetify/components'
+
 export default createVuetify({
+  // rejestrujemy standardowe komponenty + VDataTable (labs)
+  components: { ...components, VDataTable },
+  directives,
   icons: {
     defaultSet: 'mdi',
     aliases,
     sets: { mdi },
+  },
+  theme: {
+    defaultTheme: 'light',
+    themes: {
+      light: {
+        colors: {
+          primary: '#1867C0',
+          secondary: '#5CBBF6',
+        },
+      },
+    },
   },
 })
